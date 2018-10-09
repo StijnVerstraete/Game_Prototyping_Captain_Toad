00887c98ad65d04ccb88e40739e658e6e892c51a
Faust Messely <faust.messely@student.howest.be>
2018-10-09 09:14:12 +0200

    AdamsEZFix

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    GameObject[] platforms;
+    float timer;
+    [SerializeField] private float _timeStep;
+
+
+    private void Start()
+    {
+        //find all platforms with Platform tag
+        platforms = GameObject.FindGameObjectsWithTag("Platform");
+    }
+
+    private void FixedUpdate()
+    {
+        //if light is on, change opacity of all platforms to 0, and start timer
+            foreach (GameObject platform in platforms)
+            {
+                platform.GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, timer);
+            }
+        timer = Mathf.MoveTowards(timer, 0, _timeStep);
+    }
+
+    public void LightOn()
+    {
+        timer = 1;
+    }
+}
