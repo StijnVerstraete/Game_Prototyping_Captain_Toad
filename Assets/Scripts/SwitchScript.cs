4f8676e91f90684c54acc31e378f79e209b6468a
Faust Messely <faust.messely@student.howest.be>
2018-10-11 20:36:59 +0200

    light

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+
+    LightScript _light;
+    bool _inCollider=false;
+
+	// Use this for initialization
+	void Start () {
+        _light = GameObject.FindGameObjectWithTag("Light").GetComponent<LightScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_inCollider==true && Input.GetKeyDown(KeyCode.E))
+        {
+            _light.LightOn();
+        }
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag=="Player")
+        {
+            _inCollider = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            _inCollider = false;
+        }
+    }
+}
