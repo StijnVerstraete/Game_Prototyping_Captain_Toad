6153919709a8033ed01f0128d4bb1fa504b7cb85
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2018-10-16 11:14:29 +0200

    Player Prefab Updated

---
+++ Assets/Scripts/StarPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StarPickup : MonoBehaviour {
+
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
